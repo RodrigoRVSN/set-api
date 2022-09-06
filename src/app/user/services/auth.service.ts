24df469ddf9ab58040395863aeb71bd6dc52ba14
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-09-06 19:37:48 -0300

    feat: adapt payload in login service

--- src/app/user/services/auth.service.ts
+++ src/app/user/services/auth.service.ts
@@ -15,9 +15,16 @@ export class AuthService {
   }
 
   async login(user: User) {
-    const payload = { username: user.username, sub: user.id };
+    const payload = { email: user.email, sub: user.id };
+
+    const userData = {
+      name: user.role,
+      email: user.email,
+      role: user.role,
+    };
 
     return {
+      ...userData,
       access_token: this.jwtService.sign(payload),
     };
   }
