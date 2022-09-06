436b4f072ad72d6fe512bddf2456822342ec4b19
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-09-06 19:40:04 -0300

    chore: add type to validate payload in jwt strategy

--- src/app/auth/jwt.strategy.ts
+++ src/app/auth/jwt.strategy.ts
@@ -3,6 +3,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from 'src/core/utils/auth/constants';
 
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -13,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: IPayload) {
     return { userId: payload.sub, email: payload.email };
   }
 }
