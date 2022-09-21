ce22e0d572526c660529082be6acfe82c243d702
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-09-20 22:15:59 -0300

    feat: create condominiuns controllers and services

--- src/app/condominium/controllers/condominium.controller.ts
+++ src/app/condominium/controllers/condominium.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
+import { CreateCondominiumDto } from '../dtos';
 import { CondominiumService } from '../services';
 
 @Controller('condominium')
 export class CondominiumController {
   constructor(private readonly appService: CondominiumService) {}
+
+  @Get()
+  getCondominiums() {
+    return this.appService.getCondominiums();
+  }
+
+  @Post('create')
+  async createCondominium(@Body() createCondominiumDto: CreateCondominiumDto) {
+    const syndicateExists = await this.appService.findSyndicateById(
+      createCondominiumDto.syndicateId,
+    );
+
+    if (!syndicateExists) {
+      throw new BadRequestException('The user is invalid');
+    }
+
+    return this.appService.createCondominium(createCondominiumDto);
+  }
 
   @Get('syndicates')
   getSyndicates() {
--- src/app/condominium/services/condominium.service.ts
+++ src/app/condominium/services/condominium.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/core/database/db.service';
 import { Role } from 'src/core/types/IUser';
+import { CreateCondominiumDto } from '../dtos';
 
 @Injectable()
 export class CondominiumService {
   constructor(private Prisma: PrismaService) {}
+
+  getCondominiums() {
+    return this.Prisma.condominium.findMany({
+      include: {
+        syndicate: true,
+      },
+    });
+  }
 
   getSyndicates() {
     return this.Prisma.user.findMany({
@@ -14,7 +23,21 @@ export class CondominiumService {
         email: true,
         phone: true,
         role: true,
+        id: true,
       },
     });
+  }
+
+  findSyndicateById(id: string) {
+    return this.Prisma.user.findFirst({
+      where: {
+        id,
+        role: Role.SYNDICATE,
+      },
+    });
+  }
+
+  createCondominium(createCondominiumDto: CreateCondominiumDto) {
+    return this.Prisma.condominium.create({ data: createCondominiumDto });
   }
 }
