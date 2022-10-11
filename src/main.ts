5f4657549d329ab55884ed0a487f534be325439e
rodrigorvsn <rodrigovictor81@gmail.com>
2022-10-11 19:46:51 -0300

    chore: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT);
